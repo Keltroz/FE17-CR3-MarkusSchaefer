2937970feb5637df1af61b00fb41d097b63ee53f
Keltroz <death2198@gmail.com>
2022-10-14 23:08:35 +0200

    testing

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -22,8 +22,6 @@ export class CartComponent implements OnInit {
   items: Array<interfaceDishes> = [];
   // items: Array<interfaceDishes> = dishes;
 
-  result: number = 0;
-
   constructor(private cart: CartService) { }
 
   ngOnInit(): void {
