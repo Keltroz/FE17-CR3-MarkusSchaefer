01003040e6fdfc0a291c3c1f424a92bd4eb250fc
Keltroz <death2198@gmail.com>
2022-10-15 16:57:52 +0200

    finished cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -8,41 +8,55 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-
   profileForm = new FormGroup({
     firstName: new FormControl('', Validators.required),
     lastName: new FormControl('', Validators.required),
     address: new FormControl('', Validators.required),
-    phone: new FormControl('', [Validators.required, Validators.pattern("^[0-9]*$"), Validators.minLength(8)]),
-});
+    phone: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[0-9]*$'),
+      Validators.minLength(8),
+    ]),
+  });
 
   items: Array<interfaceDishes> = [];
   amount: number = 0;
   discount: number = 0;
+  total: number = 0;
+  service: number = 3.10;
 
-  constructor(private cart: CartService) { }
+  constructor(private cart: CartService) {}
 
   ngOnInit(): void {
     this.items = this.cart.getItems();
     this.amount = 0;
     this.discount = 0;
+    this.total = 0;
+    this.service = 3.10;
 
-    this.items.forEach(item => {
+    //  summary formula
+
+    
+    this.items.forEach((item) => {
       this.amount = this.amount + item.price;
     });
     console.log(this.items);
+    
+    // discount formula
+    this.total = this.service + this.amount;
 
-    if(this.amount >= 40) {
-    console.log("if is working");
-    this.discount = this.amount * 0.85;
+    if (this.amount >= 40) {
+      console.log('if is working');
+      this.total = this.amount * 0.85;
+      this.discount = this.amount * 0.15;
+    }
   }
-  }
-  
+
   onSubmit() {
-    if(this.profileForm.valid) {
+    if (this.profileForm.valid) {
       console.log(this.profileForm.value);
     }
   }
