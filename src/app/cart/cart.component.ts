4fa2bd8ee6e92f9310f7862de97416b7f27d2dab
Keltroz <death2198@gmail.com>
2022-10-15 15:55:00 +0200

    Summary formulas added

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -16,24 +16,31 @@ export class CartComponent implements OnInit {
     firstName: new FormControl('', Validators.required),
     lastName: new FormControl('', Validators.required),
     address: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', [Validators.required, Validators.pattern("^[0-9]*$"), Validators.minLength(8)]),
 });
 
   items: Array<interfaceDishes> = [];
   amount: number = 0;
+  discount: number = 0;
 
   constructor(private cart: CartService) { }
 
   ngOnInit(): void {
     this.items = this.cart.getItems();
     this.amount = 0;
+    this.discount = 0;
 
     this.items.forEach(item => {
       this.amount = this.amount + item.price;
     });
     console.log(this.items);
-  }
 
+    if(this.amount >= 40) {
+    console.log("if is working");
+    this.discount = this.amount * 0.85;
+  }
+  }
+  
   onSubmit() {
     if(this.profileForm.valid) {
       console.log(this.profileForm.value);
